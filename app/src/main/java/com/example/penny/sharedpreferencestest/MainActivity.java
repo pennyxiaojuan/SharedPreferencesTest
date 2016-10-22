69b17f631c00128d2192d09775cfc800c98c9c79
penny_xiaojuan <penny_xiaojuan@outlook.com>
2016-10-23 00:27:31 +0800

    Read data from SharePreference

--- app/src/main/java/com/example/penny/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/example/penny/sharedpreferencestest/MainActivity.java
@@ -3,17 +3,20 @@ package com.example.penny.sharedpreferencestest;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
     private Button saveData;
+    private Button restoreData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         saveData = (Button) findViewById(R.id.save_data);
+        restoreData =(Button) findViewById(R.id.restore_data);
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +29,19 @@ public class MainActivity extends Activity {
 
             }
         });
+    restoreData.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+            String name = pref.getString("name","");
+            int age = pref.getInt("age" ,0);
+            boolean married = pref.getBoolean("married",false);
+            Log.d("MainActivity","name is"+name);
+            Log.d("MainActivity","age is"+age);
+            Log.d("MainActivity","married is"+married);
+
+
+        }
+    });
     }
 }
